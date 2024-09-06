616de4a516680f81989a3159cc68f2c908b93adb
CallihanB03 <calbertley2003@gmail.com>
2024-09-06 12:35:41 -0500

    Created functions to create virtual error distributions for each participant and direction

--- virtual_err.py
+++ virtual_err.py
@@ -1,43 +1,56 @@
-from physics_python.kinematics_appr import calculate_time, calculate_velocity, sample_acceleraton
+from physics_python.kinematics_appr import calculate_time, calculate_velocity, sample_acceleration, calculate_time_change
 from physics_python.virtual_error import virtual_error
-from util.load_data import load_data
+from util.participant_data import load_participant_data
 import pandas as pd
+from torch import tensor, mean, std
 import json
 
-def existing_virtual_error():
-    df = load_data()
+def calculate_vir_err_distributions(save=False, save_path=None):
+    df = load_participant_data()
 
     participants = [int(participant[1:]) for participant in pd.unique(df["participant"])]
     participants.sort()
 
     directions = ['x', 'y', 'z']
+    virtual_error_dists = {}
 
-    virtual_error_hash = {}
     for participant in participants:
-        _, delta_t = calculate_time(df, participant)
+        virtual_error_dists[participant] = {}
+        delta_t = calculate_time_change(participant)
         for direction in directions:
-            errs = []
-            velocity = calculate_velocity(df, participant, direction)
-            acceleration = sample_acceleraton(df, participant, direction)
-            vir_err = virtual_error(velocity, acceleration, delta_t)
-            errs.append(vir_err)
-        virtual_error_hash[participant] = errs
+            loc, scale = __create_virtual_error_distribution(df, participant, direction, delta_t)
+            virtual_error_dists[participant][direction] = tensor((loc, scale))
 
-    save_path = "./data/true_virtual_error.json"
-    with open(save_path, 'w') as file:
-        json.dump(virtual_error_hash, file)
+    if save and not save_path:
+        save_path = "./data/virtual_error_distribution.json"
     
-
-
+    if save:
+        with open(save_path, 'w') as file:
+            json.dump(virtual_error_dists, file)
+    return None
     
-        
+            
+          
 
 
+def __create_virtual_error_distribution(df, participant, direction, delta_t):
+    errs = []
+    for _ in range(10):
+        acceleration = sample_acceleration(df, participant, direction)
+        velocity = calculate_velocity(acceleration, delta_t)
+        err = virtual_error(velocity, acceleration, delta_t)
+        errs.append(err)
 
+    errs = tensor(errs)
 
-    pass
+    return mean(errs).item(), std(errs).item()
 
 
 
 if __name__ == "__main__":
-    existing_virtual_error()
+    errs_dict = calculate_vir_err_distributions(save=False)
+    for first_key in errs_dict:
+        print(f"participant = {first_key}")
+        for second_key in errs_dict[first_key]:
+            print(f"    {errs_dict[first_key][second_key]}")
+        print()
