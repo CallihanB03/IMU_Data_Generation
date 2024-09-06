af283374e67f7baf9844832f77a0422fdc90de99
CallihanB03 <calbertley2003@gmail.com>
2024-09-06 11:40:49 -0500

    Renamed functions and files for more interpretable code

--- util/sampling_info.py
+++ util/sampling_info.py
@@ -1,6 +1,6 @@
 import json
 
-def download_data():
+def download_sampling_info_data():
     participant_type_1 = (3, 8)
     participant_type_2 = (4, 6, 9, 11, 13, 16, 18, 20, 22, 24)
     participant_type_3 = (i for i in range(4, 25) if (i not in participant_type_1 and i not in participant_type_2))
@@ -21,7 +21,7 @@ def download_data():
 
 
 
-def load_data(save_path=None):
+def load_sampling_info_data(save_path=None):
     if not save_path:
         save_path = "./data/sampling_info.json"
     with open(save_path, 'r') as sampling_info_data:
@@ -30,9 +30,9 @@ def load_data(save_path=None):
 
 
 if __name__ == "__main__":
-    download_data()
+    download_sampling_info_data()
 
-    sampling_info = load_data()
+    sampling_info = load_sampling_info_data()
 
     for key in sampling_info.keys():
         print(f"{key}: {sampling_info[key]}")
