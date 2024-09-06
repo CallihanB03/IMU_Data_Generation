70ba0a025a6d636a5ad4d78cb4fb128678bb7260
CallihanB03 <calbertley2003@gmail.com>
2024-09-06 12:13:59 -0500

    Set up virtual error calculation with updated functions

--- physics_python/virtual_error.py
+++ physics_python/virtual_error.py
@@ -19,13 +19,18 @@ def virtual_error(velocity_tensor, acceleration_tensor, time_window):
 
 
 if __name__ == "__main__":
-    from physics_python.kinematics_appr import calculate_time, calculate_velocity, sample_acceleraton
-    from IMU_Data_Generation.util.participant_data import load_data
+    from physics_python.kinematics_appr import calculate_time, calculate_velocity, sample_acceleration, calculate_time_change
+    from util.participant_data import load_participant_data
+    participant=3
+    direction='x'
 
 
-    df = load_data()
-    velocity, delta_t = calculate_velocity(df, participant=3, direction='x')
-    acceleration = sample_acceleraton(df, participant=3, direction='x')
+    df = load_participant_data()
+    delta_t = calculate_time_change(participant)
+    acceleration = sample_acceleration(df, participant, direction='x')
+    velocity = calculate_velocity(acceleration, delta_t)
+
+
     v_err = virtual_error(velocity, acceleration, delta_t)
     
     print(f"virtual error = {v_err}")
