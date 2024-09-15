99da883e0220e6be0cc5fafbaec0184e2116ef7f
CallihanB03 <calbertley2003@gmail.com>
2024-09-15 12:02:56 -0500

    Created files that I will use for plotting and classification models

---
+++ visualizations/latent_space_plotting.py
@@ -0,0 +1,17 @@
+from matplotlib import pyplot as plt 
+import numpy as np 
+
+
+
+def plot_latent_hist(latent_tensor):
+    #TODO:
+    # draw samples from VAE.Sampling_Layer and pass through Transformation_Distribution
+    # Plot the hist
+    pass
+
+
+def plot_latent_prob_dist(latent_tensor):
+    #TODO:
+    # draw samples from VAE.Sampling_Layer and pass through Transformation_Distribution
+    # Calculate log_prob and each point near max prob
+    pass
