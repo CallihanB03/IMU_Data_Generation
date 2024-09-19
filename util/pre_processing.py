11a1895edd0545eeda88c350d0cf1663cc7703fb
CallihanB03 <calbertley2003@gmail.com>
2024-09-19 13:56:37 -0500

    Test train split function now splits for data and label seperately

--- util/pre_processing.py
+++ util/pre_processing.py
@@ -2,23 +2,26 @@ import torch
 import random
 
 def df_to_tensor(df):
+    rows = len(df)
     if len(df.shape) == 1:
         cols = 1
-    else:
-        cols = len(df.columns)
-    rows = len(df)
+        tensor_df = torch.zeros(rows)
+        for row in range(rows):
+            tensor_df[row] = df[row]
+        return tensor_df
+    cols = len(df.columns)
     tensor_df = torch.zeros(rows, cols)
-    for col_index, col in enumerate(cols):
+    for col_index, col in enumerate(df.columns):
         for row in range(rows):
             tensor_df[row, col_index] = df[col][row]
     return tensor_df
 
 
 
-def tensor_train_test_split(tensor, test_size):
+def tensor_train_test_split(tensor_x, tensor_y, test_size):
     assert (0 <= test_size <= 1), "test_size must be in the range [0, 1]"
     train_size = 1 - test_size
-    df_num_rows = tensor.shape[0]
+    df_num_rows = tensor_x.shape[0]
 
     train_num_rows = int(train_size * df_num_rows)
     train_indices = []
@@ -34,20 +37,26 @@ def tensor_train_test_split(tensor, test_size):
     test_indices = total_indices
 
 
-    train_tensor = tensor[train_indices]
-    test_tensor = tensor[test_indices]
+    train_tensor_x = tensor_x[train_indices]
+    train_tensor_y = tensor_y[train_indices]
 
-    return train_tensor, test_tensor
+
+    test_tensor_x = tensor_x[test_indices]
+    test_tensor_y = tensor_y[test_indices]
+
+    return train_tensor_x, train_tensor_y, test_tensor_x, test_tensor_y
 
 
 if __name__ == "__main__":
     from participant_data import load_participant_data
     df = load_participant_data()
-    df = df[df.columns[:-3]] # removing non-Float values from df
-    df = df_to_tensor(df)
+    df = df[df.columns[:-2]] # removing non-Float values from df
+    data, labels = df_to_tensor(df[df.columns[:-1]]), df_to_tensor(df[df.columns[-1]])
 
-    train, test = tensor_train_test_split(df, test_size=0.2)
+    x_train, y_train, x_test, y_test = tensor_train_test_split(data, labels, test_size=0.2)
 
-    print(f"train tensor shape  = {train.shape}")
+    print(f"x_train tensor shape  = {x_train.shape}")
+    print(f"y_train tensor shape = {y_train.shape}")
     print()
-    print(f"test tensor shape = {test.shape}")
+    print(f"x_test tensor shape = {x_test.shape}")
+    print(f"y_test tensor shape = {y_test.shape}")
